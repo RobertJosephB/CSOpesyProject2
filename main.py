699a31747846a0e882ca6cb8de8a1a2ff468a1e5
RobertB <robert_brillantes@dlsu.edu.ph>
2022-06-13 11:08:36 +0800

    Added comments, modified messages, and added condition to keep adding to room if other color has no more threads

--- main.py
+++ main.py
@@ -6,26 +6,31 @@ import threading
 from time import sleep
 
 def fit_roomg():
-    global id,ctr,blue_mutex,green_mutex,room
+    # Fitting room thread for green
+    global id,ctr,room
     global n,b,g
-    global blue_done,green_done,switch ,first
+    global blue_done,green_done,switch 
    
-    print("\nGreen Thread # "+ str(id))
+   # Fitting room prints ID and color on entry
+    print("\n ID: "+ str(id) + " Color: Green")
     green_done+=1
     id+=1
+    #Critical section simulation
     sleep(3)
     room.release()
     if room._value == n:
+        #If room is empty, prints empty room
         print("\nEmpty fitting room")
     pass
     
 def fit_roomb():
-    global id,ctr,blue_mutex,green_mutex,room
+    # Fitting room thread for blue
+    global id,ctr,room
     global n,b,g
-    global blue_done,green_done,switch ,first
+    global blue_done,green_done,switch 
    
-    
-    print("\nBlue Thread # "+ str(id))
+    # Fitting room prints ID and color on entry
+    print("\n ID: "+ str(id) + " Color: Blue")
     blue_done+=1
 
     id+=1
@@ -33,24 +38,27 @@ def fit_roomb():
     sleep(3)
     room.release()
     if room._value == n:
+        #If room is empty, prints empty room
         print("\nEmpty fitting room")
     pass
 
 
 
 def green_func():
-    global id,blue_mutex,green_mutex,mtx
+    global id,mtx
     global n,b,g
-    global blue_done,green_done,switch ,first
+    global blue_done,green_done,switch
     ctr = 0
+    #Function repeats untill all green have finished
     while green_done!=g:
-
+        #Tries to acquire mutex, if other function is is using mutex, waits
         mtx.acquire()
         
         
-        #check which has the key and increase ctr for that color
+       
         temp = 1;
         while temp == 1:
+            #Acquires room and creates new thread to execute critical section
             room.acquire()
             Thread(target = fit_roomg).start()
             sleep(1)
@@ -59,8 +67,9 @@ def green_func():
             
 
             if green_done != g:
+                #If green is not finished, but value of switch has been reached by counter, waits for room to empty then releases mutex
                 if ctr==switch-1:
-                        
+                    if blue_done!=b:
                         while room._value != n:
                             pass
                         mtx.release()
@@ -69,10 +78,12 @@ def green_func():
                         ctr = 0
                         
                         
-                        
+                #Otherwise add to counter        
                 else:
                     ctr+=1
-            else :
+            #Otherwise, if all green threads done, wait for room to be empty then release mutex
+            elif green_done==g:
+
                     ctr=0
                     
                     while room._value != n:
@@ -87,27 +98,31 @@ def green_func():
     
         
 def blue_func():
-    global id,blue_mutex,green_mutex
+    global id,mtx
     global n,b,g
-    global blue_done,green_done,switch ,first
+    global blue_done,green_done,switch 
     ctr = 0
+    #Function repeats untill all blue have finished
     while blue_done!=b:
+        #Tries to acquire mutex, if other function is is using mutex, waits
         mtx.acquire()
         
     
     
-        #check which has the key and increase ctr for that color
+        
         
         temp = 1;
         while temp == 1:
+            #Acquires room and creates new thread to execute critical section
             room.acquire()
             Thread(target = fit_roomb).start()
             sleep(1)
             
             
-            
+            #If blue is not finished, but value of switch has been reached by counter, waits for room to empty then releases mutex
             if blue_done != b:
                 if ctr==switch-1:
+                    if green_done!=g:
                         while room._value != n:
                             pass
                         mtx.release()
@@ -115,8 +130,10 @@ def blue_func():
                         temp = 0
                         ctr = 0
                         
+               #Otherwise add to counter        
                 else:
                     ctr+=1
+            #Otherwise, if all blue threads done, wait for room to be empty then release mutex
             elif blue_done==b:
                 ctr=0
                 while room._value != n:
